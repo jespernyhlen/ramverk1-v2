51b107e1e0c153e99f7b7a4fdf042faf07a3d86f
Jesper Nyhlen <jeppe_nyhlen@hotmail.com>
2019-12-01 16:48:52 +0800

    Adding report for kmom04

--- config/navbar/responsive.php
+++ config/navbar/responsive.php
@@ -35,6 +35,11 @@ return [
                         "text" => "Kmom03",
                         "url" => "redovisning/kmom03",
                         "title" => "Redovisning för kmom03.",
+                    ],
+                    [
+                        "text" => "Kmom04",
+                        "url" => "redovisning/kmom04",
+                        "title" => "Redovisning för kmom04.",
                     ]
                 ],
             ],
